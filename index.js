0ead783b93698fde76779ec237da5514a0de09d5
programingchicken <aquacrazz.gp@gmail.com>
2021-08-13 10:26:19 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const sequelize = require('./database');
+
+sequelize.sync().then(() => console.log('db is ready'))
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("app is running");
+});
